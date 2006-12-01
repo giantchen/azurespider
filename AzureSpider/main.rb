b74644dccee232d9c7bc5b539d3770ea53ed53df
giantchen <giantchen@3b0ebeb0-8c23-0410-9944-a3e8113b79f9>
2006-12-01 15:59:10 +0000

    start use Hpricot.

--- AzureSpider/main.rb
+++ AzureSpider/main.rb
@@ -1,3 +1,10 @@
 require 'rubygems'
 require 'hpricot'
 
+doc = open("2001.shtml") { |f| Hpricot(f) }
+
+(doc/"a").each do |link|
+  if link['href'] =~ /00.shtml$/
+    puts link['href'], link.inner_html
+  end
+end
