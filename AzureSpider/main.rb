5952d369dd7ac76d510d457927447d0934aa8d88
giantchen <giantchen@3b0ebeb0-8c23-0410-9944-a3e8113b79f9>
2006-12-04 16:48:46 +0000

    dangdang txt pages

--- AzureSpider/main.rb
+++ AzureSpider/main.rb
@@ -1,10 +1,71 @@
+#!/usr/bin/env ruby
 require 'rubygems'
 require 'hpricot'
 
-doc = open("2001.shtml") { |f| Hpricot(f) }
-
-(doc/"a").each do |link|
-  if link['href'] =~ /00.shtml$/
-    puts link['href'], link.inner_html
+def get1()
+  doc = open("2001_u8.html") { |f| Hpricot(f) }
+  (doc/"a").each do |link|
+    if link['href'] =~ /00.shtml$/
+      puts "http://www.dangdang.com" + link['href']
+    end
   end
 end
+
+def get2()
+  Dir.glob('01*.shtml').each do |fn|
+#    puts fn
+    found = false
+    doc = open(fn) {|f| Hpricot(f)}
+    doc.search("a").each do |link|
+      if link.inner_html =~ /strong/
+        #puts link['href'].split('/')[4]
+        puts "http://www.dangdang.com" + link['href']
+        found = true
+      end
+    end
+    if (!found) 
+#      puts "******"
+    end
+  end
+end
+
+def get3()
+  Dir.glob('txt/01.*.shtml').each do |fn|
+  #["txt/01.01.01.00.00.00txt.shtml"].each do |fn|
+    fn_base = fn.split('/')[1][0..19]
+    puts fn_base
+    max = 0
+    maxurl = ""
+    doc = open(fn) {|f| Hpricot(f)}
+    doc.search("a").each do |link|
+      if link.inner_html =~ /^\d+$/
+        page = link['href'].sub(".shtml", "").split('_')[1].to_i
+        if page > max
+          max = page
+          maxurl = link['href']
+        end
+        #puts link['href']# + ", " + link.inner_html
+      elsif link['href'].include?(fn_base + "_") && link.inner_html != ">>"
+        puts link['href'] + ", " + link.inner_html + "**"
+      end
+    end  
+
+    (2..max).each do |page|
+      puts maxurl.sub("_#{max}", "_#{page}")
+    end
+  end  
+end
+
+def get4
+  Dir.glob('txt/01.*.shtml').each do |fn|
+    #puts fn
+    doc = open(fn) {|f| Hpricot(f)}
+    doc.search("a").each do |link|
+      if link['href'] =~ /product/
+        puts "http://www.dangdang.com" + link['href']
+      end
+    end    
+  end  
+end
+
+get4()
---
+++ AzureSpider/site_dangdang.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+
+SITE_URL = "http://www.dangdang.com"
+
+def putu(url, text)
+  printf("%s%s\t%s\n", SITE_URL, url, text)
+end
+
+def get_all_txt_pages(txt_urls)
+  txt_urls.each_key do |txturl|
+    url = SITE_URL + txturl
+    max = 0
+    maxurl = ""
+    fn_base = txturl.sub(".shtml", "")
+    puts url
+    
+    doc = Hpricot(open(url))
+    doc.search("a").each do |link|
+      if link.inner_html =~ /^\d+$/
+        page = link['href'].sub(".shtml", "").split('_')[1].to_i
+        if page > max
+          max = page
+          maxurl = link['href']
+        end
+        #puts link['href']# + ", " + link.inner_html
+      elsif link['href'].include?(fn_base + "_") && link.inner_html != ">>"
+        puts link['href'] + ", " + link.inner_html + "**"
+      end
+    end  
+
+    (2..max).each do |page|
+      puts SITE_URL + maxurl.sub("_#{max}", "_#{page}")
+    end
+
+  end
+end
+
+def get_base_txt_pages
+  #doc = Hpricot(open("http://www.dangdang.com/zhuanti2006/book/2001.shtml"))
+  txt_urls = Hash.new
+  doc = Hpricot(open("2001.shtml"))
+  doc.search("a[@href]").each do |link|
+    href = link['href']
+    if (href.include? '00.shtml') && (!href.include? '.00.00.00.00.shtml')
+      txturl = href.sub(".shtml", "txt.shtml")
+      # putu txturl, link.inner_html
+      txt_urls[txturl] = 1
+    end
+  end
+  puts txt_urls.length
+  get_all_txt_pages(txt_urls)
+end
+
+get_base_txt_pages
