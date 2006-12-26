c3c1a52101992a5433685f728560560ef0e63da3
giantchen <giantchen@3b0ebeb0-8c23-0410-9944-a3e8113b79f9>
2006-12-26 18:53:17 +0000

    get details of a book.

--- trunk/AzureSpider/site_dangdang.rb
+++ trunk/AzureSpider/site_dangdang.rb
@@ -72,11 +72,68 @@ def get_book_url
   end
 end
 
-def get_book_details(file)
-  doc = Hpticot(open(file))
-  
+class Book
+  def initialize(file)
+    @regexps = {
+      'title' => /productname="(.*)"/,
+      'author' => /作者：<\/span><span class="black">(.*?)<\/a>/,
+      'publisher' => /出版社：<\/span><a href='.*'>(.*?)<\/a>/,
+      'isbn' => /ISBN：<\/span><span class="black">(.*?)<\/span>/,
+      'publish_date' => /出版日期：<\/span><span class="black">(.*?)<\/span>/,
+      'version' => /版次：<\/span><span class="black">(.*?)<\/span>/,
+      'price' => /productstr1="定价：(.*?)元/,
+      'site_price' => /productstr1=".*当当价：(.*?)元/,
+      'vip_price' => /productstr1=".*钻石VIP价：(.*?)元/,
+      'summary' => /内容提要：<\/td>\s*<\/tr>\s*<tr>\s*<td>(.*?)<\/td>/,
+      'author_intro' => /作者简介：<\/td>\s*<\/tr>\s*<tr>\s*<td>(.*?)<\/td>/
+    }
+    @filters = {
+      'summary' => lambda {|text| unhtml(text)},
+      'author_intro' => lambda {|text| unhtml(text)},
+    }
+    @values = {}
+    parse(file)
+  end
+
+  def to_s
+    # "title: #{@values[:title]}\n" + "author: #{@values[:author]}\n"
+    s = ''
+    @values.sort.each {|x| s << x.join(": ") << "\n"}
+    s
+  end
+
+ private
+
+  def parse(file)
+    text = open(file).gets(nil)
+    @regexps.each do |key, re|
+      if text =~ re
+        if @filters.key?(key)
+          @values[key] = @filters[key].call($1.strip)
+        else
+          @values[key] = $1.strip
+        end
+      end
+    end
+  end
+
+  def unhtml(s)
+    if (s)
+      s.gsub(/&nbsp;/, ' ')#.gsub(/<br>/, "\n")
+    else
+      nil
+    end
+  end
 end
+
 # get_base_txt_pages
 # get_all_txt_pages('01.47.07.00.00.00txt.shtml'=>1, '01.54.04.00.00.00txt.shtml' => 1)
 
-#get_book_url
+File.open("b2.txt").each_line do |file|
+  file.chomp!.sub!(/^http:\//, '.')
+  if File.exist? file
+    book = Book.new(file)
+    #printf '.'
+    puts file, book
+  end
+end
