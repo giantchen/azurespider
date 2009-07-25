c354efe14caca78c4230581ed1adb6cf1f97ed36
giantchen <giantchen@3b0ebeb0-8c23-0410-9944-a3e8113b79f9>
2009-07-25 09:29:19 +0000

    Add base class Strategy, for calculating trading costs.

--- trunk/phenom/src/phenom/PriceData.java
+++ trunk/phenom/src/phenom/PriceData.java
@@ -2,39 +2,75 @@ package phenom;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class PriceData {
-	private static final String SQL = "select Open, Close from STOCK_PRICE where Symbol = '600001.sh' and Date >= '20080101' and Date <= '20081230'";
+public final class PriceData {
+	private static final String SQL = "SELECT Date, Open, High, Low, Close, Amount, Volume, Exchange "
+			+ " FROM STOCK_PRICE WHERE Symbol = ? AND Exchange = ? ORDER BY Date";
+
+	static private String dbPath = "data/superT_STOCK.sqlite";
+	static private String scon = "jdbc:sqlite:" + dbPath;
 
 	Map<String, Stock> stocks;
 
-	public PriceData() throws Exception {
-		stocks = new HashMap<String, Stock>();
-		Class.forName("org.sqlite.JDBC");
-		String dbPath = "data/superT_STOCK.sqlite";
-		String scon = "jdbc:sqlite:" + dbPath;
-		Connection conn = DriverManager.getConnection(scon);
-		Statement s = conn.createStatement();
-		ResultSet rs = s.executeQuery(SQL);
-		List<Double> openPrices = new ArrayList<Double>();
-		List<Double> closePrices = new ArrayList<Double>();
-		while (rs.next()) {
-			double open = rs.getDouble("Open") / 1000.0;
-			double close = rs.getDouble("Close") / 1000.0;
-			openPrices.add(open);
-			closePrices.add(close);
+	private Connection conn;
+
+	public PriceData() {
+		try {
+			stocks = new HashMap<String, Stock>();
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection(scon);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		rs.close();
-		stocks.put("600001.sh", new Stock(openPrices, closePrices));
 	}
 
-	public Stock getStockPrice(String id) {
-		return stocks.get(id);
+	/**
+	 * @param symbol
+	 */
+	private void readFromDatabase(String id) {
+		try {
+			String[] fields = id.split("\\.");
+			String symbol = fields[0];
+			String exchange = fields[1];
+			PreparedStatement s = conn.prepareStatement(SQL);
+			s.setString(1, symbol);
+			s.setString(2, exchange);
+			ResultSet rs = s.executeQuery();
+			List<String> dates = new ArrayList<String>();
+			List<Double> openPrices = new ArrayList<Double>();
+			List<Double> highPrices = new ArrayList<Double>();
+			List<Double> lowPrices = new ArrayList<Double>();
+			List<Double> closePrices = new ArrayList<Double>();
+			List<Double> amounts = new ArrayList<Double>();
+			List<Long> volumnes = new ArrayList<Long>();
+
+			while (rs.next()) {
+				dates.add(rs.getString("Date"));
+				openPrices.add(rs.getDouble("Open") / 1000.0);
+				highPrices.add(rs.getDouble("High") / 1000.0);
+				lowPrices.add(rs.getDouble("Low") / 1000.0);
+				closePrices.add(rs.getDouble("Close") / 1000.0);
+				amounts.add(rs.getDouble("Amount"));
+				volumnes.add(rs.getLong("Volume"));
+			}
+			rs.close();
+			stocks.put(id, new Stock(symbol, exchange, dates, openPrices,
+					highPrices, lowPrices, closePrices, amounts, volumnes));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Stock getStock(String symbol) {
+		if (!stocks.containsKey(symbol)) {
+			readFromDatabase(symbol);
+		}
+		return stocks.get(symbol);
 	}
 }
--- trunk/phenom/src/phenom/Stock.java
+++ trunk/phenom/src/phenom/Stock.java
@@ -5,20 +5,40 @@ import java.util.List;
 
 public class Stock {
 
-	private double[] closePrices;
+	private String symbol;
+	private String exchange;
+	private String[] dates;
 	private double[] openPrices;
+	private double[] highPrices;
+	private double[] lowPrices;
+	private double[] closePrices;
+	private double[] amounts;
+	private long[] volumes;
 
-	public Stock(List<Double> open, List<Double> close) {
+	public Stock(String sy, String ex, List<String> date, List<Double> open,
+			List<Double> high, List<Double> low, List<Double> close,
+			List<Double> amount, List<Long> volume) {
+		symbol = sy;
+		exchange = ex;
+		dates = date.toArray(new String[0]);
 		openPrices = listToArray(open);
+		highPrices = listToArray(high);
+		lowPrices = listToArray(low);
 		closePrices = listToArray(close);
+		amounts = listToArray(amount);
+		volumes = listToArray(volume);
 	}
 
-	private double[] listToArray(List<Double> list) {
-		double[] array = new double[list.size()];
-		for (int i = 0; i < array.length; ++i) {
-			array[i] = list.get(i);
-		}
-		return array;
+	public String getSymbol() {
+		return symbol;
+	}
+
+	public String getExchange() {
+		return exchange;
+	}
+
+	public String[] getDates() {
+		return Arrays.copyOf(dates, dates.length);
 	}
 
 	/**
@@ -28,10 +48,39 @@ public class Stock {
 		return Arrays.copyOf(openPrices, openPrices.length);
 	}
 
-	/**
-	 * Deep copy of price.
-	 */
+	public double[] getHighPrices() {
+		return Arrays.copyOf(highPrices, highPrices.length);
+	}
+
+	public double[] getLowPrices() {
+		return Arrays.copyOf(lowPrices, lowPrices.length);
+	}
+
 	public double[] getClosePrices() {
 		return Arrays.copyOf(closePrices, closePrices.length);
+	}
+
+	public double[] getAmounts() {
+		return Arrays.copyOf(amounts, amounts.length);
+	}
+
+	public long[] getVolumnes() {
+		return Arrays.copyOf(volumes, volumes.length);
+	}
+
+	private static double[] listToArray(List<Double> list) {
+		double[] array = new double[list.size()];
+		for (int i = 0; i < array.length; ++i) {
+			array[i] = list.get(i);
+		}
+		return array;
+	}
+
+	private static long[] listToArray(List<Long> list) {
+		long[] array = new long[list.size()];
+		for (int i = 0; i < array.length; ++i) {
+			array[i] = list.get(i);
+		}
+		return array;
 	}
 }
